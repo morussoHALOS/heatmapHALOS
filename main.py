b0f26809ec4bba189fe0b543386c28a1807a87d2
Mo <mo.russolillo@halosbodycams.com>
2025-05-16 11:03:32 -0500

    Update spreadsheet name in main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(creds)
-sheet = client.open("realGeocoded").sheet1
+sheet = client.open("Heatmap Master Sheet HALOS").sheet1
 
 data = sheet.get_all_records()
 
